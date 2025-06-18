e0c764363d12293c9811c0718a6833251ff0f3c2
DouglasGomes2 <gomedouglas200@gmail.com>
2025-06-18 11:50:36 -0300

    layouts Criar e Editar Atualizados

--- resources/views/livewire/clientes/create.blade.php
+++ resources/views/livewire/clientes/create.blade.php
@@ -1,73 +1,91 @@
 <div>
     <x-button :text="__('Criar Novo Cliente')" wire:click="$toggle('modal')" sm />
 
-    <x-modal :title="__('Criar Novo Cliente')" wire x-on:open="setTimeout(() => $refs.name.focus(), 250)">
+    <x-modal :title="__('Criar Novo Cliente')" wire x-on:open="setTimeout(() => $refs.name.focus(), 250)" size="6xl" blur>
         <form id="cliente-create" wire:submit="save" class="space-y-4">
-            <div>
-                @if ($imagemTemp)
-                    <img src="{{ $imagemTemp->temporaryUrl() }}" alt="" width="200" height="200">
-                @elseif($cliente?->foto)
-                    <img src="{{ 'storage/' . $cliente?->foto }}" alt="" width="200" height="200">
-                @else
-                    <img src="{{ asset('assets/images/no-image.png') }}" alt="" width="200" height="200">
-                @endif
+            <div class="grid md:grid-cols-12 md:gap-4">
+                <div class="md:col-span-3">
+                    @if ($imagemTemp)
+                        <img src="{{ $imagemTemp->temporaryUrl() }}" alt="">
+                    @elseif($cliente?->foto)
+                        <img src="{{ 'storage/' . $cliente?->foto }}" alt="">
+                    @else
+                        <img src="{{ asset('assets/images/no-image.png') }}" alt="" class="rounded-lg">
+                    @endif
 
-                @if ($errors->get('imagemTemp'))
-                    <span id="nome-error" class="error invalid-feedback d-block">{{ $errors->first('imagemTemp') }}</span>
-                @endif
-                @if ($errors->get('cliente.foto'))
-                    <span id="nome-error"
-                        class="error invalid-feedback d-block">{{ $errors->first('cliente.foto') }}</span>
-                @endif
+                    @if ($errors->get('imagemTemp'))
+                        <span id="nome-error"
+                            class="error invalid-feedback d-block">{{ $errors->first('imagemTemp') }} </span>
+                    @endif
+                    @if ($errors->get('cliente.foto'))
+                        <span id="nome-error"
+                            class="error invalid-feedback d-block">{{ $errors->first('cliente.foto') }}</span>
+                    @endif
 
-            </div>
 
-            <div>
-                <x-upload close-after-upload label="Screenshot" hint="We need to analyze your screenshot"
-                    tip="Drag and drop your screenshot here" wire:model='imagemTemp' required :preview="false" />
-            </div>
-
-            <div>
-                <x-select.styled label="{{ __('Tags') }} *" placeholder="Selecione uma Tag"
-                    wire:model="tags_selecionadas" :options="$tags" select="label:nome|value:id" multiple />
-            
-                </div>
-            <div x-data="{ tipoPessoa: '' }" x-init="tipoPessoa = 'F'">
-                <div class="mb-4">
-                    <x-select.native label="{{ __('Tipo Pessoa') }} *" wire:model="cliente.tipo_pessoa"
-                        :options="[['name' => 'Fisica', 'id' => 'F'], ['name' => 'Juridica', 'id' => 'J']]" select="label:name|value:id" x-model="tipoPessoa" required />
+                    <div>
+                        <x-upload close-after-upload
+                            tip="Arraste e Solte a imagem aqui" wire:model='imagemTemp' required
+                            :preview="false" />
+                    </div>
                 </div>
 
-                <div>
-                    <x-input label="{{ __('CPF/CNPJ') }} *" wire:model="cliente.cpf_cnpj" required
-                        x-mask:dynamic="tipoPessoa === 'J' ||tipoPessoa === '' ? '99.999.999/9999-99' : '999.999.999-99'" />
+                <div class="md:col-span-9">
+                    <div x-data="{ tipoPessoa: '' }" x-init="tipoPessoa = 'F'" class="grid md:grid-cols-6 md:gap-4">
+                        <div>
+                            <x-select.native label="{{ __('Tipo Pessoa') }} *" wire:model="cliente.tipo_pessoa"
+                                :options="[['name' => 'Fisica', 'id' => 'F'], ['name' => 'Juridica', 'id' => 'J']]" select="label:name|value:id" x-model="tipoPessoa" required />
+                        </div>
+
+                        <div class="md:col-span-2">
+                            <x-input label="{{ __('CPF/CNPJ') }} *" wire:model="cliente.cpf_cnpj" required
+                                x-mask:dynamic="tipoPessoa === 'J' ||tipoPessoa === '' ? '99.999.999/9999-99' : '999.999.999-99'" />
+                        </div>
+
+                        <div class="md:col-span-3">
+                            <x-input label="{{ __('Nome') }} *" x-ref="nome" wire:model="cliente.nome" required />
+                        </div>
+                    </div>
+
+                <div class="grid md:grid-cols-3 md:gap-4">
+                    <div>
+                        <x-input label="{{ __('Email') }} *" wire:model="cliente.email" required />
+                    </div>
+                    <div>
+                        <x-input x-mask="(99) 99999-9999" label="{{ __('Telefone') }} *" wire:model="cliente.telefone"
+                            required />
+                    </div>
+                    <div>
+                        <x-date format="DD/MM/YYYY" label="{{ __('Nascimento') }}" wire:model="cliente.nascimento"
+                            required />
+                    </div>
                 </div>
-            </div>
-            <div>
-                <x-input label="{{ __('Nome') }} *" x-ref="nome" wire:model="cliente.nome" required />
-            </div>
-            <div>
-                <x-input label="{{ __('Email') }} *" wire:model="cliente.email" required />
-            </div>
-            <div>
-                <x-input x-mask="(99) 99999-9999" label="{{ __('Telefone') }} *" wire:model="cliente.telefone"
-                    required />
-            </div>
-            <div>
-                <x-date format="DD/MM/YYYY" label="{{ __('Nascimento') }}" wire:model="cliente.nascimento" required />
-            </div>
-            <div>
-                <x-currency mutate locale="pt-BR" symbol="R$" label="{{ __('Crédito') }}"
-                    wire:model="cliente.credito" required />
-            </div>
-            <div>
-                <x-toggle label="Crédito Ativo" wire:model="cliente.credito_ativo" :checked="$cliente?->credito_ativo ? true : false" />
-            </div>
-            <div>
-                <x-select.native label="{{ __('Status') }} *" wire:model="cliente.status" :options="[['name' => 'Ativo', 'id' => 'A'], ['name' => 'Inativo', 'id' => 'I']]"
-                    select="label:name|value:id" required />
+                <div class="grid md:grid-cols-3 md:gap-4">
+                    <div>
+                        <x-currency mutate locale="pt-BR" symbol="R$" label="{{ __('Crédito') }}"
+                            wire:model="cliente.credito" required />
+                    </div>
+                    <div class="md:pt-8">
+                        <x-toggle label="Crédito Ativo" wire:model="cliente.credito_ativo" :checked="$cliente?->credito_ativo ? true : false" />
+                    </div>
+                    <div>
+                        <x-select.native label="{{ __('Status') }} *" wire:model="cliente.status" :options="[['name' => 'Ativo', 'id' => 'A'], ['name' => 'Inativo', 'id' => 'I']]"
+                            select="label:name|value:id" required />
+
+                    </div>
+                </div>
+                    <div>
+                        <x-select.styled label="{{ __('Tags') }} *" placeholder="Selecione uma Tag"
+                            wire:model="tags_selecionadas" :options="$tags" select="label:nome|value:id" multiple />
+
+                    </div>
+                </div>
 
             </div>
+
+
+
+
         </form>
         <x-slot:footer>
             <x-button type="submit" form="cliente-create">
--- resources/views/livewire/clientes/update.blade.php
+++ resources/views/livewire/clientes/update.blade.php
@@ -1,69 +1,82 @@
 <div>
-    <x-modal :title="__('Editar Cliente', ['id' => $cliente?->id])" wire x-on:open="setTimeout(() => $refs.name.focus(), 250)">
+    <x-modal :title="__('Editar Cliente', ['id' => $cliente?->id])" wire x-on:open="setTimeout(() => $refs.name.focus(), 250)" blur size="6xl">
         <form id="cliente-update-{{ $cliente?->id }}" wire:submit="save" class="space-y-4">
-            <div>
-                @if ($imagemTemp)
-                    <img src="{{ $imagemTemp->temporaryUrl() }}" alt="" width="200" height="200">
-                @elseif($cliente?->foto)
-                    <img src="{{ 'storage/' . $cliente?->foto }}" alt="" width="200" height="200">
-                @else
-                    <img src="{{ asset('assets/images/no-image.png') }}" alt="" width="200" height="200">
-                @endif
+            <div class="grid md:grid-cols-12 md:gap-4">
+                <div class="md:col-span-3">
+                    @if ($imagemTemp)
+                        <img src="{{ $imagemTemp->temporaryUrl() }}" alt="" >
+                    @elseif($cliente?->foto)
+                        <img src="{{ 'storage/' . $cliente?->foto }}" alt="" >
+                    @else
+                        <img src="{{ asset('assets/images/no-image.png') }}" alt="" class="rounded-lg" >
+                    @endif
 
-                @if ($errors->get('imagemTemp'))
-                    <span id="nome-error" class="error invalid-feedback d-block">{{ $errors->first('imagemTemp') }}</span>
-                @endif
-                @if ($errors->get('cliente.foto'))
-                    <span id="nome-error"
-                        class="error invalid-feedback d-block">{{ $errors->first('cliente.foto') }}</span>
-                @endif
-
-            </div>
-            <div>
-                <x-upload close-after-upload label="Screenshot" hint="We need to analyze your screenshot"
-                    tip="Drag and drop your screenshot here" wire:model='imagemTemp' required :preview="false" />
-            </div>
-
-            <div>
-                <x-select.styled  label="{{ __('Tags') }} *" placeholder="Selecione uma Tag" wire:model="tags_selecionadas" :options="$tags" select="label:nome|value:id" searchable multiple />
-            </div>
-            
-            <div x-data="{ tipoPessoa: '{{ $cliente?->tipo_pessoa }}' }">
-                <div class="mb-4">
-                    <x-select.native x-model="tipoPessoa" label="{{ __('Tipo Pessoa') }} *"
-                        wire:model="cliente.tipo_pessoa" :options="[['name' => 'Fisica', 'id' => 'F'], ['name' => 'Juridica', 'id' => 'J']]" select="label:name|value:id" required />
+                    @if ($errors->get('imagemTemp'))
+                        <span id="nome-error"
+                            class="error invalid-feedback d-block">{{ $errors->first('imagemTemp') }}</span>
+                    @endif
+                    @if ($errors->get('cliente.foto'))
+                        <span id="nome-error"
+                            class="error invalid-feedback d-block">{{ $errors->first('cliente.foto') }}</span>
+                    @endif
+                    <div>
+                        <x-upload close-after-upload tip="Arraste e Solte a imagem aqui" wire:model='imagemTemp'
+                            required :preview="false" />
+                    </div>
                 </div>
-                <div>
-                    <x-input label="{{ __('CPF/CNPJ') }} *" wire:model="cliente.cpf_cnpj" required
-                        x-mask:dynamic="tipoPessoa === 'J' ||tipoPessoa === '' ? '99.999.999/9999-99' : '999.999.999-99'" />
+
+                <div class="md:col-span-9">
+                    <div x-data="{ tipoPessoa: '{{ $cliente?->tipo_pessoa }}' }" class="grid md:grid-cols-6 md:gap-4">
+                        <div>
+                            <x-select.native x-model="tipoPessoa" label="{{ __('Tipo Pessoa') }} *"
+                                wire:model="cliente.tipo_pessoa" :options="[['name' => 'Fisica', 'id' => 'F'], ['name' => 'Juridica', 'id' => 'J']]" select="label:name|value:id"
+                                required />
+                        </div>
+                        <div class="md:col-span-2">
+                            <x-input label="{{ __('CPF/CNPJ') }} *" wire:model="cliente.cpf_cnpj" required
+                                x-mask:dynamic="tipoPessoa === 'J' ||tipoPessoa === '' ? '99.999.999/9999-99' : '999.999.999-99'" />
+                        </div>
+
+                        <div class="md:col-span-3">
+                            <x-input label="{{ __('Nome') }} *" x-ref="nome" wire:model="cliente.nome" required />
+                        </div>
+                    </div>
+                    <div class="grid md:grid-cols-3 md:gap-4">
+                        <div>
+                            <x-input label="{{ __('Email') }} *" wire:model="cliente.email" required />
+                        </div>
+                        <div>
+                            <x-input x-mask="(99) 99999-9999" label="{{ __('Telefone') }} *"
+                                wire:model="cliente.telefone" required />
+                        </div>
+                        <div>
+                            <x-date format="DD/MM/YYYY" label="{{ __('Nascimento') }}" wire:model="cliente.nascimento"
+                                required />
+                        </div>
+                    </div>
+                    <div class="grid md:grid-cols-3 md:gap-4">
+                        <div>
+                            <x-currency mutate locale="pt-BR" symbol="R$" label="{{ __('Crédito') }}"
+                                wire:model="cliente.credito" required />
+                        </div>
+                        <div class="md:pt-8">
+                            <x-toggle label="Crédito Ativo" wire:model="cliente.credito_ativo" :checked="$cliente?->credito_ativo ? true : false" />
+
+                        </div>
+                        <div>
+                            <x-select.native label="{{ __('Status') }} *" wire:model="cliente.status"
+                                :options="[['name' => 'Ativo', 'id' => 'A'], ['name' => 'Inativo', 'id' => 'I']]" select="label:name|value:id" required />
+                        </div>
+                    </div>
+
+                    <div>
+                        <x-select.styled label="{{ __('Tags') }} *" placeholder="Selecione uma Tag"
+                            wire:model="tags_selecionadas" :options="$tags" select="label:nome|value:id" searchable
+                            multiple />
+                    </div>
                 </div>
             </div>
-            <div>
-                <x-input label="{{ __('Nome') }} *" x-ref="nome" wire:model="cliente.nome" required />
-            </div>
-            <div>
-                <x-input label="{{ __('Email') }} *" wire:model="cliente.email" required />
-            </div>
-            <div>
-                <x-input x-mask="(99) 99999-9999" label="{{ __('Telefone') }} *" wire:model="cliente.telefone"
-                    required />
-            </div>
-            <div>
-                <x-date format="DD/MM/YYYY" label="{{ __('Nascimento') }}" wire:model="cliente.nascimento" required />
-            </div>
-            <div>
-                <x-currency mutate locale="pt-BR" symbol="R$" label="{{ __('Crédito') }}"
-                    wire:model="cliente.credito" required />
-            </div>
 
-
-            <div>
-                <x-toggle label="Crédito Ativo" wire:model="cliente.credito_ativo" :checked="$cliente?->credito_ativo ? true : false" />
-            </div>
-            <div>
-                <x-select.native label="{{ __('Status') }} *" wire:model="cliente.status" :options="[['name' => 'Ativo', 'id' => 'A'], ['name' => 'Inativo', 'id' => 'I']]"
-                    select="label:name|value:id" required />
-            </div>
         </form>
         <x-slot:footer>
             <x-button type="submit" form="cliente-update-{{ $cliente?->id }}">
