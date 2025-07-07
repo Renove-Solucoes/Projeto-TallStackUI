5a746063c7d68601ead7894a093015615bd383f4
DouglasGomes2 <gomedouglas200@gmail.com>
2025-07-07 17:06:16 -0300

    Wip SideBar Colors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use TallStackUi\Facades\TallStackUi;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,20 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        //
+        $blocks = [
+            'desktop.wrapper.first.size',
+            'desktop.wrapper.second',
+            'desktop.wrapper.third',
+            'desktop.wrapper.fourth',
+            'desktop.wrapper.fifth',
+            'desktop.collapse.wrapper',
+
+        ];
+
+        $sidebar = TallStackUi::personalize()->sideBar();
+
+        foreach ($blocks as $block) {
+            $sidebar->block($block, 'bg-dark-700');
+        }
     }
 }
