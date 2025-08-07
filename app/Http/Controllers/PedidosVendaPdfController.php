ef3aa2b5f922daccbde70a2b398a844ce33d740f
DouglasGomes2 <gomedouglas200@gmail.com>
2025-08-07 16:35:27 -0300

    PDF Pedido de vendas

--- app/Http/Controllers/PedidosVendaPdfController.php
+++ app/Http/Controllers/PedidosVendaPdfController.php
@@ -14,6 +14,8 @@ class PedidosVendaPdfController
     public function generatePdf(PedidosVenda $pedidosVenda)
     {
         $pdf = Pdf::loadView('pdf.pedidos-venda', ['pedido' => $pedidosVenda]);
+
+
         return $pdf->stream("pedido_{$pedidosVenda->id}.pdf");
     }
 }
