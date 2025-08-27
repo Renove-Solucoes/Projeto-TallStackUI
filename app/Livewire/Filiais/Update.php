b2f780e342aabdd3ffaca52a743792f18b898065
DouglasGomes2 <gomedouglas200@gmail.com>
2025-08-27 11:24:39 -0300

    Filial Crud

--- app/Livewire/Filiais/Update.php
+++ app/Livewire/Filiais/Update.php
@@ -2,12 +2,86 @@
 
 namespace App\Livewire\Filiais;
 
+use App\Livewire\Traits\Alert;
+use App\Models\Empresa;
+use App\Models\Filial;
+use Livewire\Attributes\On;
 use Livewire\Component;
+use Illuminate\Support\Facades\Log;
+use TallStackUi\Traits\Interactions;
+use Illuminate\Validation\Rule;
 
 class Update extends Component
 {
+    use Alert;
+    use Interactions;
+
+    public Filial $filial;
+
+    public $empresas;
+
+    public $modal = false;
+
+
+    #[On('load::filial')]
+    public function load(Filial $filial): void
+    {
+        $this->filial = $filial;
+        $this->modal = true;
+    }
+
+    public function mount()
+    {
+        $this->empresas = Empresa::all(['id', 'nome'])
+            ->map(fn($empresa) => [
+                'id' => $empresa->id,
+                'nome' => $empresa->nome,
+            ])
+            ->toArray();
+    }
+
+    public function rules()
+    {
+        return [
+            'filial.empresa_id' => ['required', 'exists:empresas,id'],
+            'filial.razao_social' => ['required', 'string', 'max:60', Rule::unique('filials', 'razao_social')->ignore($this->filial->id)],
+            'filial.nome_fantasia' => ['required', 'string', 'max:60', Rule::unique('filials', 'nome_fantasia')->ignore($this->filial->id)],
+            'filial.status' => ['required', 'string', 'max:1'],
+            'filial.tipo_pessoa' => ['required', 'string', 'max:1'],
+        ];
+    }
+
+
     public function render()
     {
         return view('livewire.filiais.update');
+    }
+
+
+    public function save()
+    {
+        $this->validate();
+
+        try {
+            $this->filial->save();
+            $this->dispatch('updated');
+
+            $this->reset();
+
+            $this->toast()->success('Atenção!', 'Filial atualizada com sucesso.')->send();
+
+            $this->empresas = Empresa::all(['id', 'nome'])
+                ->map(fn($empresa) => [
+                    'id' => $empresa->id,
+                    'nome' => $empresa->nome,
+                ])
+                ->toArray();
+        } catch (\Exception $e) {
+            Log::error('Erro ao atualizar filial - User ID: ' . auth()->user()->id . ' nome: ' . auth()->user()->name, [
+                'message' => $e->getMessage(),
+                'exception' => $e,
+            ]);
+            $this->error('Atenção!', 'Não foi possível atualizar a filial.');
+        }
     }
 }
--- resources/views/livewire/filiais/update.blade.php
+++ resources/views/livewire/filiais/update.blade.php
@@ -1,3 +1,31 @@
 <div>
-    {{-- The whole world belongs to you. --}}
+    <x-modal :title="__('Update Filial: #:id', ['id' => $filial?->id])" wire x-on:open="setTimeout(() => $refs.name.focus(), 250)" size="xl" blu>
+        <form id="categoria-update-{{ $filial?->id }}" wire:submit="save" class="space-y-4">
+            <div class="grid md:grid-cols-12 md:gap-4">
+                <div class="md:col-span-3">
+                    <x-select.native label="{{ __('Tipo') }} *" wire:model="filial.tipo_pessoa" :options="[['name' => 'fisico', 'id' => 'F'], ['name' => 'Juridico', 'id' => 'J']]"
+                        select="label:name|value:id" required />
+                </div>
+                <div class="md:col-span-4">
+                    <x-input label="{{ __('Razão social') }} *" x-ref="nome" wire:model="filial.razao_social" />
+                </div>
+                <div class="md:col-span-4">
+                    <x-input label="{{ __('Nome Fantasia') }} *" x-ref="nome" wire:model="filial.nome_fantasia" />
+                </div>
+                <div class="md:col-span-3">
+                    <x-select.native label="{{ __('Status') }} *" wire:model="filial.status" :options="[['name' => 'Ativo', 'id' => 'A'], ['name' => 'Inativo', 'id' => 'I']]"
+                        select="label:name|value:id" required />
+                </div>
+                <div class="md:col-span-6">
+                    <x-select.native label="{{ __('Empresa') }} *" wire:model="filial.empresa_id" :options="$empresas"
+                        select="label:nome|value:id" required placeholder="{{ __('Selecione uma Empresa') }}" />
+                </div>
+            </div>
+        </form>
+        <x-slot:footer>
+            <x-button type="submit" form="categoria-update-{{ $filial?->id }}">
+                @lang('Save')
+            </x-button>
+        </x-slot:footer>
+    </x-modal>
 </div>
